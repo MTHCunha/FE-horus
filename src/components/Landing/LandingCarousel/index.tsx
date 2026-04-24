d57a4c94ce6a8e3a179d189f5217efe7735ab9c9
MTHCunha <mthcunha2003@gmail.com>
2026-04-24 10:00:30 -0300

    fix: carousel_time

--- src/components/Landing/LandingCarousel/index.tsx
+++ src/components/Landing/LandingCarousel/index.tsx
@@ -7,7 +7,7 @@ import Content from "./Content";
 
 const LandingCarousel = () => {
   return (
-    <Carousel autoplay={{ dotDuration: true }} autoplaySpeed={555554000}>
+    <Carousel autoplay={{ dotDuration: true }} autoplaySpeed={4000}>
       <CarouselContent backgroundImage={team}>
         <Content
           buttonText="About Us"
